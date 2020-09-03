092de81f8c88bd0d7fd21302b763a7cef90f4ddf
Kayla Wood <kaylaewood@gmail.com>
2020-09-03 16:40:19 -0400

    Add expandability to sections

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-console.log('hello');
+var page = document.querySelector('body');
+
+page.addEventListener('click', function() {
+  if (event.target.closest('nav') && event.target.closest('nav').classList.contains('section-heading')) {
+    event.target.closest('section').children[1].classList.toggle('hidden');
+    if (event.target.closest('nav').children[1].style.backgroundImage === 'url("resources/shrink.png")') {
+      event.target.closest('nav').children[1].style.backgroundImage = 'url("resources/expand.png")';
+    } else {
+      event.target.closest('nav').children[1].style.backgroundImage = 'url("resources/shrink.png")';
+    }
+  }
+});
