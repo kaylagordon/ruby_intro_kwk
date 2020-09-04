1ccf2eaa157e607f7f0ad33850bb1dc580b118c1
Kayla Wood <kaylaewood@gmail.com>
2020-09-04 10:44:42 -0400

    Address children error

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ document.querySelector('body').addEventListener('click', collapseContent);
 function collapseContent() {
   let navBar = event.target.closest('nav');
   let contentSection = event.target.closest('section').children[1];
-  let collapseButton = navBar.children[1];
+  let collapseButton = event.target.closest('nav').children[1];
   if (navBar && navBar.classList.contains('section-heading')) {
     contentSection.classList.toggle('hidden');
     if (collapseButton.style.backgroundImage === 'url("resources/shrink.png")') {
