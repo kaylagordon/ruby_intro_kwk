29c0e990ae009a91dba14a64e169c4eba6e94ee7
Kayla Wood <kaylaewood@gmail.com>
2020-09-06 15:11:43 -0400

    Add styling to goals completion

--- main.js
+++ main.js
@@ -2,14 +2,25 @@ document.querySelector('body').addEventListener('click', collapseContent);
 
 function collapseContent() {
   let navBar = event.target.closest('nav');
-  let contentSection = event.target.closest('section').children[1];
-  let collapseButton = event.target.closest('nav').children[1];
+
   if (navBar && navBar.classList.contains('section-heading')) {
+    let contentSection = event.target.closest('section').children[1];
+    let collapseButton = event.target.closest('nav').children[1];
     contentSection.classList.toggle('hidden');
     if (collapseButton.style.backgroundImage === 'url("resources/shrink.png")') {
       collapseButton.style.backgroundImage = 'url("resources/expand.png")';
     } else {
       collapseButton.style.backgroundImage = 'url("resources/shrink.png")';
     }
+  }
+}
+
+document.querySelector('.learning-goals').addEventListener('click', completeGoal);
+
+function completeGoal() {
+  if (event.target.classList.contains('goal-checkbox') && event.target.closest('div').children[1].classList.contains('complete')) {
+    event.target.closest('div').children[1].classList.remove('complete');
+  } else if (event.target.classList.contains('goal-checkbox')) {
+    event.target.closest('div').children[1].classList.add('complete');
   }
 }
